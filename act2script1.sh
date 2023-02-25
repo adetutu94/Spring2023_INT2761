847cbc0c468bbce4bcacb4571f0aa3d0bbf0cc9e
adetutu94 <125331040+adetutu94@users.noreply.github.com>
2023-02-25 12:27:15 -0500

    Created act2script1.sh
    Basic test script using if/then

---
+++ act2script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Setting variable
+read -p "What class are you in now: " class
+
+#Test script from online act
+if [ class = Linux ]
+then 
+ echo "Welcome to weekend Saturday. Am doing GitHub."
+ fi
